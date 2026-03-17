691e19dc8190e3e7df1beb155f7a1c1731a895c5
nukeop <12746779+nukeop@users.noreply.github.com>
2026-03-17 16:45:09 +0100

    Game clock host implementation

---
+++ packages/game/src/renderer/hosts/gameClockHost.ts
@@ -0,0 +1,12 @@
+import type { GameClockHost } from '@asteroid-miner/mod-sdk';
+
+import { useGameClockStore } from '../stores/useGameClockStore';
+
+export const createGameClockHost = (): GameClockHost => ({
+  getTurn: () => useGameClockStore.getState().turn,
+
+  advanceDay: () => useGameClockStore.getState().advanceDay(),
+
+  subscribe: (listener) =>
+    useGameClockStore.subscribe((state) => listener(state.turn)),
+});
