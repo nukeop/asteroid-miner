a480116dac8ce143185f54671077d713971b24b5
nukeop <12746779+nukeop@users.noreply.github.com>
2026-03-28 02:43:34 +0100

    Start loading data before intializing

--- packages/game/src/renderer/main.tsx
+++ packages/game/src/renderer/main.tsx
@@ -5,6 +5,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
+import { initializeDefinitionsStore } from './stores/useDefinitionsStore';
+
+initializeDefinitionsStore();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
